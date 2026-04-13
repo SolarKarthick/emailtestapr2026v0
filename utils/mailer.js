7b0fae680106caabaf671e73ce7aaeef3165b601
SolarKarthick <your-email@gmail.com>
2026-04-13 21:41:13 +0530

    check for email connection v0

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,17 +2,13 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for 465, false for 587
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  connectionTimeout: 10000, // 10 sec
-  greetingTimeout: 10000,
-  socketTimeout: 10000
+  }
 });
-
 
 
 
